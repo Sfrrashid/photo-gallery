7ccd5f9d5638f116224adc0f0a31ecd9d977bd89
Saifur Rashid <sfrrashid@gmail.com>
2021-05-24 10:25:39 +0600

    navigation bar produce

---
+++ src/MainComponent/MainComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Home from '../Component/Home/Home';
+import Gallery from '../Component/Gallery/Gallery';
+
+
+const MainComponent = () => {
+
+    return (
+        <div>
+            <Home />
+            <Gallery />
+
+        </div>
+
+
+    );
+
+
+}
+export default MainComponent;
